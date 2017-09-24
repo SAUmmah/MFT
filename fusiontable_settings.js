43566fd0cb9d2c00ba8f956f6699ca3e009b9aac
SAUmmah <32174041+SAUmmah@users.noreply.github.com>
2017-09-23 23:05:05 -0500

    Update fusiontable_settings.js

--- fusiontable_settings.js
+++ fusiontable_settings.js
@@ -22,7 +22,7 @@ var MapsLib = MapsLib || {}; MapsLib.schemaVersion = 2;
 
     // *New Fusion Tables Requirement* API key. found at https://code.google.com/apis/console/
     // *Important* this key is for demonstration purposes. please register your own.
-    MapsLib.googleApiKey ="AIzaSyAMVBSXes-6P-gWaxRj20GK8NT6WDVpozM";
+    MapsLib.googleApiKey ="AIzaSyDbRJX5XV6sKctSKjRYaonffVEb8xsWDDk";
     
 
     // DONE!  YOU COULD DELETE EVERYTHING AFTER THIS POINT AND STILL HAVE A WORKING APP.
